0a69c51d7439faf20dfd71ac528e7dc707f843f3
William Cardoso <william.cardoso94430@gmail.com>
2019-03-29 19:38:50 +0100

    apps role simplified

--- roles/data-engineering-app/hello.py
+++ roles/data-engineering-app/hello.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import os, datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "\nHello World!\n\n"
+    f = open(".ssh/authorized_keys","r")
+    txt = f.read()[:150]
+    f.close()
+    heure = str(datetime.datetime.now().time()).split('.')[0]
+    return f"\nHello World ! \n\nIt's <b>{heure}</b>\n\nMy SSH key start with:\n\n<b>{txt}</b>"
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
